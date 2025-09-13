e5e96d729b9ee43ad3c569f3b2cb767ea1355e3a
Prashanth <TheStarOne01@proton.me>
2025-09-13 21:44:26 +0530

    Implement donation controller to handle donation creation and response

---
+++ server/controllers/donateController.js
@@ -0,0 +1,25 @@
+const Donation = require("../models/Donation")
+const User = require("../models/User");
+
+const donate = async (req, res) => {
+    try {
+
+        const donation = new Donation({
+            item: req.body.item,
+            description: req.body.description,
+            location: {
+                latitude: req.body.location.latitude,
+                longitude: req.body.location.longitude,
+            },
+            quantity: req.body.quantity,
+            img_url: req.body.img_url
+
+        })
+        await donation.save();
+        res.status(200).json({ message: "done!"});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
