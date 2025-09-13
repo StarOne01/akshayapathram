a9f7bb8a9e805f74ed8bb9214798864967c7171f
Prashanth <TheStarOne01@proton.me>
2025-09-13 22:29:00 +0530

    Add donate routes to server configuration

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 const authMiddleware = require("./middleware/authMiddleware");
+const donateRoutes = require("./routes/donateRoutes");
 
 const app = express();
 const port = 3000;
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch((err) => console.log(err));
 
 app.use("/", authRoutes);
+app.use("/", donateRoutes);
 
 app.get("/protected", authMiddleware, (req, res) => {
     res.json({ message: "Protected data", user: req.user });
